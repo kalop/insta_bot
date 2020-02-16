74695b316197cc09b8f341f72aa1061c763d4986
Albert <albertcabrerizo@gmail.com>
2020-02-16 19:30:07 +0100

    load hashtags and iterate

--- insta_bot.py
+++ insta_bot.py
@@ -2,6 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 from secrets import username, password
+from random import randint
+from hashtags import hashtags
+
+# https://gist.github.com/tacomonster/555bceef3d14673810f625edd000c112
+
+WAIT_TIME = randint(2, 5)
 
 
 class InstaBot():
@@ -9,64 +15,71 @@ class InstaBot():
         self.driver = webdriver.Chrome()
         self.action = ActionChains(self.driver)
 
+    def _getLink(self, link):
+        self.driver.get(link)
+        sleep(WAIT_TIME)
+        self.action = ActionChains(self.driver)
+
     def login(self):
-        self.driver.get('https://www.instagram.com/')
+        try:
+            self._getLink('https://www.instagram.com/')
 
-        sleep(2)
+            login_link = self.driver.find_element_by_xpath(
+                '//*[@id="react-root"]/section/main/article/div[2]/div[2]/p/a')
+            login_link.click()
 
-        login_link = self.driver.find_element_by_xpath(
-            '//*[@id="react-root"]/section/main/article/div[2]/div[2]/p/a')
-        login_link.click()
+            sleep(WAIT_TIME)
 
-        sleep(2)
+            username_in = self.driver.find_element_by_xpath(
+                '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')
+            username_in.send_keys(username)
 
-        username_in = self.driver.find_element_by_xpath(
-            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')
-        username_in.send_keys(username)
+            passw_in = self.driver.find_element_by_xpath(
+                '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')
+            passw_in.send_keys(password)
 
-        passw_in = self.driver.find_element_by_xpath(
-            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')
-        passw_in.send_keys(password)
+            login_btn = self.driver.find_element_by_xpath(
+                '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div')
+            login_btn.click()
 
-        login_btn = self.driver.find_element_by_xpath(
-            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div')
-        login_btn.click()
+            sleep(WAIT_TIME)
 
-        sleep(3)
+            notNow_btn = self.driver.find_element_by_xpath(
+                '/html/body/div[4]/div/div/div[3]/button[2]')
+            notNow_btn.click()
 
-        notNow_btn = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/div/div/div[3]/button[2]')
-        notNow_btn.click()
+            sleep(WAIT_TIME)
 
-        sleep(3)
+        except Exception as error:
+            print(str(error))
+            return False
+        return True
 
     def hashtag_search(self, hashtag):
         search = self.driver.find_element_by_xpath(
             '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[2]')
-        sleep(2)
+        sleep(WAIT_TIME)
         search.click()
 
         search_in = self.driver.find_element_by_xpath(
             '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
         search_in.send_keys(hashtag)
-        sleep(2)
+        sleep(WAIT_TIME)
         list_item = self.driver.find_element_by_xpath(
             '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]/div')
 
         list_item.click()
 
     def go_to_hashtag_link(self, hashtag):
-        self.driver.get(
-            'https://www.instagram.com/explore/tags/%s/' % hashtag)
-        sleep(2)
+        self._getLink('https://www.instagram.com/explore/tags/%s/' % hashtag)
 
     def get_pics(self):
         pic_hrefs = []
-        for i in range(1, 3):
+        for _ in range(1, 7):
             try:
                 self.driver.execute_script(
                     "window.scrollTo(0, document.body.scrollHeight);")
-                sleep(2)
+                sleep(WAIT_TIME)
                 hrefs_in_view = self.driver.find_elements_by_tag_name('a')
                 # finding relevant hrefs
                 hrefs_in_view = [elem.get_attribute('href') for elem in hrefs_in_view
@@ -83,27 +96,36 @@ class InstaBot():
             '/html/body/div[4]/div[3]/button')
         cross.click()
 
-    def like_pic(self):
+    def like_pic_from_link(self, link):
+        self._getLink(link)
         pic = self.driver.find_element_by_xpath(
-            '/html/body/div[4]/div[2]/div/article/div[1]/div/div/div[1]/div[2]')
+            '//*[@id="react-root"]/section/main/div/div/article/div[1]/div')
         self.action.double_click(pic).perform()
-        sleep(2)
+        sleep(WAIT_TIME)
+
+    def perform_hashtag(self, hashtag):
+        self.go_to_hashtag_link(hashtag)
+        pics = self.get_pics()
+        for link in pics:
+            try:
+                self.like_pic_from_link(link)
+                # self.perform_profile()
+            except Exception as e:
+                print(str(e))
+                continue
+
+    def perform_profile(self):
+        pass
+
+    def load_hashtags(self):
+        for hashtag in hashtags:
+            self.perform_hashtag(hashtag)
+            sleep(10)
 
 
 if __name__ == "__main__":
     bot = InstaBot()
-    bot.login()
-    bot.go_to_hashtag_link('motoadventure')
-    bot.scroll_down()
-    # bot.like_pic()
-    # bot.click_next_pic()
-    # bot.like_pic()
-    # bot.click_next_pic()
-    # bot.like_pic()
-    # bot.go_to_hashtag_link('motorcycle')
-    # bot.open_first_pic()
-    # bot.like_pic()
-    # bot.click_next_pic()
-    # bot.like_pic()
-    # bot.click_next_pic()
-    # bot.like_pic()
+    while not bot.login():
+        print('trying to login...')
+
+    bot.load_hashtags()
